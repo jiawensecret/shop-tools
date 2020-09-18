18d2f897901faa19c41c5386bff77e340e138474
Gone with the wind <294128448@qq.com>
2020-09-18 17:23:07 +0800

    新修改

--- app/Services/SaleVolume.php
+++ app/Services/SaleVolume.php
@@ -136,9 +136,11 @@ class SaleVolume
                             $supportTender = SupportPriceTender::where('sku', $item['sku_deal'])->first();
                         }
 
-                        $aPrice = $supportTender['price'] ?? 0;
+//                        $aPrice = $supportTender['price'] ?? 0;
+//
+//                        $supplierPrice = $item->supplier_price ?: $aPrice;
 
-                        $supplierPrice = $item->supplier_price ?: $aPrice;
+                        $supplierPrice = $supportTender['price'] ?? $item->supplier_price;
                         $cost += $item->count * $supplierPrice;
                     }
 
