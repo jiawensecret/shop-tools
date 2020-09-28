e9c7b263b4323b4920a5529ea96ab6723a4c85a8
Gone with the wind <294128448@qq.com>
2020-09-28 19:00:16 +0800

    新修改

--- app/Services/SaleVolume.php
+++ app/Services/SaleVolume.php
@@ -97,8 +97,11 @@ class SaleVolume
 //                if (!$supportTender) {
 //                    $supportTender = SupportPriceTender::where('sku', $item['sku_deal'])->first();
 //                }
+                $aPrice = $supportTender['price'] ?? 0;
 
-                $supplierPrice = $supportTender['price'] ?? $item->supplier_price;
+                $supplierPrice = $item->supplier_price ?: $aPrice;
+
+               //$supplierPrice = $supportTender['price'] ?? $item->supplier_price;
                 $cost += $item->count * $supplierPrice;
                 unset($supportTender);
             }
@@ -139,11 +142,11 @@ class SaleVolume
 //                            $supportTender = SupportPriceTender::where('sku', $item['sku_deal'])->first();
 //                        }
 
-//                        $aPrice = $supportTender['price'] ?? 0;
-//
-//                        $supplierPrice = $item->supplier_price ?: $aPrice;
+                        $aPrice = $supportTender['price'] ?? 0;
 
-                        $supplierPrice = $supportTender['price'] ?? $item->supplier_price;
+                        $supplierPrice = $item->supplier_price ?: $aPrice;
+
+                        //$supplierPrice = $supportTender['price'] ?? $item->supplier_price;
 
                         $cost = $cost + $item['count'] * $supplierPrice;
                         unset($supportTender);
