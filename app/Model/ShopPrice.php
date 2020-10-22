1f6180c8d34e39400e3af0c86f85921990051897
Gone with the wind <294128448@qq.com>
2020-10-22 12:02:10 +0800

    新修改

--- app/Model/ShopPrice.php
+++ app/Model/ShopPrice.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class ShopPrice extends Model
 {
     protected $guarded = ['id'];
+
+    protected $appends = ['shop_name'];
+
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class);
+    }
+
+    public function getShopNameAttribute($value)
+    {
+        return $this->shop->name ?? '';
+    }
 }
