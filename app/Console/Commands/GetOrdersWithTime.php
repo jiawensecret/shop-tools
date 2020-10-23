d02524a29c4d9070341cc41e8eff962da792e80e
Gone with the wind <294128448@qq.com>
2020-10-23 15:21:41 +0800

    新修改

---
+++ app/Console/Commands/GetOrdersWithTime.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Model\Shop;
+use App\Services\Shopify;
+use App\SystemShopifyLog;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class GetOrdersWithTime extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'get:orders {--shop=} {--start_time=} {--end_time=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '通过shopify api获取订单';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $shop_id = $this->option('shop');
+        $start_time = $this->option('start_time');
+        $end_time = $this->option('end_time');
+
+        $shop = Shop::find($shop_id);
+
+        try {
+            $model = new Shopify($shop);
+
+            [$data,$url] = $model->getOrders(Carbon::now()->subMonths(3)->toDateTimeString());
+
+            foreach($data as $item){
+                $time = Carbon::parse($item['created_at'])->toDateTimeString();
+                if($time >= $start_time && $time <= $end_time) {
+                    $model->dealOrder($item);
+                }
+            }
+            while($url) {
+                $shopifyLog = SystemShopifyLog::create([
+                    'url' => $url,
+                    'command' => $this->signature,
+                    'shop_id' => $shop->id,
+                    'pid' => 0
+                ]);
+                [$data,$url] = $model->getOrdersByUrl($url,$shopifyLog);
+
+                foreach($data as $item){
+                    $time = Carbon::parse($item['created_at'])->toDateTimeString();
+                    if($time >= $start_time && $time <= $end_time) {
+                        $model->dealOrder($item);
+                    }
+                }
+            }
+        } catch (\Exception $exception) {
+            Log::error($exception->getMessage());
+        }
+
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Console\Commands\GetOrdersFromShopify;
 use App\Console\Commands\GetOrdersTest;
+use App\Console\Commands\GetOrdersWithTime;
 use App\Console\Commands\initSku;
 use App\Console\Commands\SetSupplierPrice;
 use App\Console\Commands\ShopifyPayment;
@@ -24,7 +25,8 @@ class Kernel extends ConsoleKernel
         ShopifyShipping::class,
         SetSupplierPrice::class,
         initSku::class,
-        GetOrdersTest::class
+        GetOrdersTest::class,
+        GetOrdersWithTime::class,
     ];
 
     /**
